3733ff84bb3166e3dd2acdaa9e2a0574cfe56ebc
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-05-22 12:07:30 -0400

    Updated component to make template logic work.

--- addon/components/bread-crumbs.js
+++ addon/components/bread-crumbs.js
@@ -3,13 +3,16 @@ import layout from '../templates/components/bread-crumbs';
 
 const get = Ember.get;
 const {
+  A: emberArray,
   EnumerableUtils,
   Component,
   Logger,
   computed,
+  on,
   getWithDefault,
   assert,
-  typeOf
+  typeOf,
+  setProperties
 } = Ember;
 
 const {
@@ -23,32 +26,64 @@ const {
 
 export default Component.extend({
   layout: layout,
-  outputStyle: 'bootstrap',
-  linkable: true,
-  currentRouteName: computed.alias('applicationController.currentRouteName'),
+  tagName: 'ol',
+  classNameBindings: [ 'breadCrumbClass' ],
+  hasBlock: computed.bool('template').readOnly(),
+  currentRouteName: computed.readOnly('applicationController.currentRouteName'),
 
   routeHierarchy: computed('currentRouteName', {
     get() {
-      const container = get(this, 'container');
-      const currentRouteName = getWithDefault(this, 'currentRouteName', '');
+      const currentRouteName = getWithDefault(this, 'currentRouteName', false);
 
-      if (currentRouteName === '') {
-        warn('No current route found');
-      }
+      assert('[ember-crumbly] Could not find a curent route', currentRouteName);
 
       const routeNames = this._splitCurrentRouteName(currentRouteName);
       const filteredRouteNames = this._filterIndexRoutes(routeNames);
 
-      return this._lookupBreadCrumb(routeNames, filteredRouteNames, container);
+      return this._lookupBreadCrumb(routeNames, filteredRouteNames);
     },
 
     set() {
-      warn('`routeHierarchy` is read only');
+      warn('[ember-crumbly] `routeHierarchy` is read only');
     }
+  }),
+
+  breadCrumbClass: computed('outputStyle', {
+    get() {
+      let className = 'breadcrumb';
+      const outputStyle = getWithDefault(this, 'outputStyle', '');
+      const lowerCaseOutputStyle = outputStyle.toLowerCase();
+
+      if (lowerCaseOutputStyle === 'foundation') {
+        className = 'breadcrumbs';
+      }
+
+      return className;
+    },
+
+    set() {
+      warn('[ember-crumbly] `breadCrumbClass` is read only');
+    }
+  }),
+
+  _setDefaults: on('init', function() {
+    setProperties(this, {
+      linkable: true
+    });
   }),
 
   _splitCurrentRouteName(currentRouteName) {
     return currentRouteName.split('.');
+  },
+
+  _guessRoutePath(routeNames, name, index) {
+    const routes = routeNames.slice(0, index + 1);
+
+    if (routes.length === 1) {
+      return `${name}.index`;
+    } else {
+      return routes.join('.');
+    }
   },
 
   _filterIndexRoutes(routeNames) {
@@ -57,35 +92,35 @@ export default Component.extend({
     });
   },
 
-  _lookupBreadCrumb(routeNames, filteredRouteNames, container) {
+  _lookupRoute(routeName) {
+    const container = get(this, 'container');
+    const route = container.lookup(`route:${routeName}`);
+    assert(`[ember-crumbly] \`route:${routeName}\` was not found`, route);
+
+    return route;
+  },
+
+  _lookupBreadCrumb(routeNames, filteredRouteNames) {
+    const defaultLinkable = get(this, 'linkable');
     const breadCrumbs = map(filteredRouteNames, (name, index) => {
-      let path;
-      let breadCrumb;
-      const routes = routeNames.slice(0, index + 1);
-
-      if (routes.length === 1) {
-        path = `${name}.index`;
-      } else {
-        path = routes.join('.');
-      }
-
-      const routeObject = container.lookup(`route:${path}`);
-      assert(`\`route:${path}\` was not found`, routeObject);
-
-      breadCrumb = routeObject.getWithDefault('breadCrumb', {});
+      const path = this._guessRoutePath(routeNames, name, index);
+      const breadCrumb = this._lookupRoute(path).getWithDefault('breadCrumb', {});
       const breadCrumbType = typeOf(breadCrumb);
 
       if (breadCrumbType === 'null') {
         return;
       } else {
-        breadCrumb.path = path;
+        setProperties(breadCrumb, {
+          path,
+          linkable: breadCrumb.hasOwnProperty('linkable') ? breadCrumb.linkable : defaultLinkable
+        });
       }
 
       return breadCrumb;
     });
 
-    return filter(breadCrumbs, (breadCrumb) => {
+    return emberArray(filter(breadCrumbs, (breadCrumb) => {
       return typeOf(breadCrumb) !== 'undefined';
-    });
+    }));
   }
 });
