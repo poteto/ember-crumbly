2d90bc255ede20afecebf7e7d8a51b3c87ecaf05
rakeshkumar-jha <rakeshkumar.jha@freshworks.com>
2020-09-22 10:06:10 +0530

    engine support

--- addon/initializers/crumbly.js
+++
@@ -1,9 +0,0 @@
-export function initialize() {
-  const application = arguments[1] || arguments[0];
-  application.inject('component:bread-crumbs', 'applicationRoute', 'route:application');
-}
-
-export default {
-  name: 'crumbly',
-  initialize
-};
